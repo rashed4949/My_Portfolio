b584d20a3aeee38e7ffb8a56a7cc09c28549c82e
rashed4949 <rrashed4@gmail.com>
2023-12-27 07:54:36 +0100

    CV and About me page update

--- src/components/about/AboutMeBio.jsx
+++ src/components/about/AboutMeBio.jsx
@@ -8,7 +8,7 @@ const AboutMeBio = () => {
   return (
     <div className="block sm:flex sm:gap-10 mt-10 sm:mt-20">
       <div className="w-full sm:w-1/2 mb-7 sm:mb-0">
-        <img src={profileImage} className="rounded-lg w-96 mx-40" alt="" />
+        <img src={profileImage} className="rounded-lg w-96" alt="" />
       </div>
 
       <div className="font-general-regular w-full sm:w-3/4 text-left">
