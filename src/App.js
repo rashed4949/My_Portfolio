ca49d949129cf8c79a66204063dfaa1c81bd92a3
rashed4949 <rrashed4@gmail.com>
2024-10-22 16:10:48 +0200

    refactor: tab project name

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import "./css/App.css";
 import { AnimatePresence } from "framer-motion";
 import { lazy, Suspense } from "react";
-import { BrowserRouter, Router, Route, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import AppHeader from "./components/shared/AppHeader";
 import "./css/App.css";
 
@@ -15,29 +15,16 @@ function App() {
   return (
     <AnimatePresence>
       <div className=" bg-secondary-light dark:bg-primary-dark transition duration-300">
-        {/* <ScrollToTop /> */}
-
         <AppHeader />
         <Suspense fallback={""}>
           <Routes>
             <Route path="/" element={<Home />} />
-            {/* <Route path="projects" element={<Projects />} />
-            <Route
-              path="projects/single-project"
-              element={<ProjectSingle />}
-            />
-
-            <Route path="about" element={<About />} />
-            <Route path="contact" element={<Contact />} /> */}
             <Route path="about" element={<About />} />
             <Route path="profiles" element={<Profiles />} />
             <Route path="projects" element={<Projects />} />
-            {/* <Route path="projects/single-project" element={<ProjectSingle />} /> */}
             <Route path="contact" element={<Contact />} />
           </Routes>
         </Suspense>
-        {/* <AppFooter /> */}
-        {/* <UseScrollToTop /> */}
       </div>
     </AnimatePresence>
   );
